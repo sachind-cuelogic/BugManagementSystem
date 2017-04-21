e5075428d76d0216fde8fc1a48002c5e18740923
Sachin Dane <sachin.dane@cuelogic.com>
2017-04-21 14:21:42 +0530

    disable dependent module field

--- bms/bms_app/static/bms_app/js/bug_view.js
+++ bms/bms_app/static/bms_app/js/bug_view.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	if(document.getElementById('message')!==null){
 		$.notify.defaults({ className: "success" })
 		$.notify( 
-			"Blog details saved successfully",
+			"You have successfully created bug",
 			{ position:"top center" }
 			);
 	}
